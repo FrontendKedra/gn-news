0cfbed978057565937a8a390054b15c39fa77507
Szymon Kedra <s5zee@wp.pl>
2023-03-27 18:25:38 +0200

    added an object with messages for polish and english language

---
+++ src/common/Locale/messages.ts
@@ -0,0 +1,106 @@
+export const messages = {
+  pl: {
+    alt: "Brak zdjęcia",
+    header: {
+      popup: "Opis procesu tworzenia",
+      view: "Zmień widok wyświetlanych newsów",
+      localeChanger: "Zmień język",
+    },
+    articles: {
+      t: {
+        existantT: "Tytuł artykułu:",
+        nonExistantT: "Brak tytułu",
+      },
+      teaser: {
+        existantTeaser: "Opis:",
+        nonExistantTeaser: "Autor:",
+      },
+      source: {
+        existantSource: "Nazwa źródła:",
+        nonExistantSource: "Brak źródła",
+      },
+      publicationDate: "Data publikacji:",
+    },
+    error: {
+      header: "Coś poszło nie tak...",
+      message1: "Sprawdź połączenie z internetem",
+      message2: "i spróbuj ponownie",
+      linkButton: "Wróć na stronę główną",
+    },
+    loader: {
+      paragraph1: "Proszę czekać,",
+      paragraph2: "newsy są ładowane...",
+    },
+    footer: "Liczba artykułów:",
+    pagination: {
+      previous: "Poprzednia",
+      next: "Następna",
+      page: "Strona",
+      of: "z",
+    },
+    sideMenu: "Brak zdjęcia",
+    popup: {
+      author: "Autor:",
+      content: "Brak artykułu ze źródła:",
+      text: "Link do artykułu:",
+      creationProcess: `Największe trudności sprawił mi popup ze względu na to, ze wcześniej
+    nie miałem z nim do czynienia a także dobranie odpowiedniej flagi do
+    nazwy państwa ze względu na problem z działaniem emoji flag na
+    windowsie. Najciekawiej tworzyło się odpowiednie warunki w przypadku
+    braku jakiegoś pola z api oraz dodawało możliwość zmiany języka.`,
+    },
+  },
+  en: {
+    alt: "No Image",
+    header: {
+      popup: "Creation process description",
+      view: "Change view of the news",
+      localeChanger: "Change language",
+    },
+    articles: {
+      t: {
+        existantT: "Article name:",
+        nonExistantT: "No title",
+      },
+      teaser: {
+        existantTeaser: "Description:",
+        nonExistantTeaser: "Author:",
+      },
+      source: {
+        existantSource: "Source name:",
+        nonExistantSource: "No source",
+      },
+      publicationDate: "Publication date:",
+    },
+    error: {
+      header: "Something went wrong...",
+      message1: "Check your network connection",
+      message2: "and try again",
+      linkButton: "Back to home page",
+    },
+    loader: {
+      paragraph1: "Please wait,",
+      paragraph2: "news are being loaded...",
+    },
+    footer: "Number of articles:",
+    pagination: {
+      previous: "Previous",
+      next: "Next",
+      page: "Page",
+      of: "of",
+    },
+    sideMenu: "No Image",
+    popup: {
+      author: "Author:",
+      content: "No articles from this source:",
+      text: "Article link:",
+      creationProcess: `The most troublesome was the popup because I never
+      had to deal with it before and also finding the right flag for
+      the country name was problematic due to the issues related to 
+      displaying emoji flags on windows.
+      The thing I enjoyed the most was setting the right conditions in case of
+      missing a certain field from the api and adding a language change feature.
+      `,
+    },
+  },
+};
