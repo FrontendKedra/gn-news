53c56578f98e538408b506c2353cc5beb1828d9e
Szymon Kedra <s5zee@wp.pl>
2023-03-27 18:05:34 +0200

    App added

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { HashRouter, Redirect, Route, Switch } from "react-router-dom";
+import { Header } from "./features/Header";
+import { SideMenu } from "./features/SideMenu";
+import { Main } from "./features/Main";
+import { Footer } from "./features/Footer";
+import { AppContainer } from "./features/AppContainer/styled";
+
+function App() {
+  return (
+    <HashRouter>
+      <AppContainer>
+        <Header />
+        <SideMenu />
+        <Footer />
+        <Switch>
+          <Route path="/country/:id">
+            <Main />
+          </Route>
+          <Route path="/">
+            <Redirect to="/country/pl" />
+          </Route>
+        </Switch>
+      </AppContainer>
+    </HashRouter>
+  );
+}
+
+export default App;
